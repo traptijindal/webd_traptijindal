fb5cb81ff375ca27287b5995570a5a00987ac27a
traptijindal <traptijindal5678@gmail.com>
2023-10-15 23:31:49 +0530

    changes

---
+++ script.js
@@ -0,0 +1,88 @@
+let totalAmount=document.getElementById("total-amount");
+let userAmount=document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+const productTitle =document.getElementById("product-title");
+const errorMessage= document.getElementById("budget-error");
+const productTitleError = document.getElementById("product-title-error");
+const productCostError =document.getElementById("product-cost-error");
+const amount=document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+const list = document.getElementById("list");
+let tempAmount= 0;
+totalAmountButton.addEventListener("click",()=>{
+    tempAmount=totalAmount.value;
+    if(tempAmount === ""||tempAmount<0){
+        errorMessage.classList.remove("hide");
+    }
+    else{
+        errorMessage.classList.add("hide");
+        amount.innerHTML = tempAmount;
+        balanceValue.innerText= tempAmount-expenditureValue.innerText;
+        totalAmount.value="";
+    }
+});
+
+const disableButtons=(bool)=>{
+    let editButtons=document.getElementsByClassName("edit");
+    Array.from(editButtons).forEach((element )=>{
+        element.disable = bool;
+    });
+};
+const modifyElement=(element,edit=false)=>{
+    let parentDiv = element.parent.Element;
+    let currentBalance = balanceValue.innerText;
+    let currentExpanse = expenditureValue.innerText;
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
+    if(edit){
+        let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
+        userAmount.value=parentAmount;
+        disableButtons(true);
+    }
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+    expenditureValue.innerText = parseInt(currentExpanse)-parseInt(parentAmount);
+    parentDiv.remove();
+};
+
+const listCreator = (expenseName,expenseValue) => {
+    let sublistContent  = document.createElement("div");
+    sublistContent.classList.add("sublist-content","flex-space");
+    list.appendChild(sublistContent);
+    sublistContent.innerHTML =`<p class ="product">$
+    {expenseName}</p><p class = "amount">$
+    {expenseValue}</p>`;
+    let editButton = document.createElement("button");
+    editButton.classList.add("fa fa-pencil-square-o");
+    editButton.style.fontSize="24px";
+    editButton.add.EventListener("click",()=> {modifyElement(editButton,true)});
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("fa fa-minus-circle","delete");
+    deleteButton.style.fontSize ="24px";
+    deleteButton.addEventListener("click",() =>{
+        modifyElement(deleteButton);
+    });
+    sublistContent.appendChild(editButton);
+    sublistContent.append(deleteButton);
+    document.getElementById("list").appendChild(sublistContent);
+};
+checkAmountButton.addEventListener("click",() => {
+    if(!userAmount.value||!productTitle.value){ 
+    productTitleError.classList.remove("hide");
+    return false;
+}
+   disableButtons(false);
+   let expenditure = parseInt(userAmount.value);
+   let sum = parseInt(expenditureValue.innerText) + expenditure;
+   expenditureValue.innerText = sum;
+   const totalBalance = tempAmount - sum;
+   balanceValue.innerText = totalBalance;
+   listCreator(productTitle.value,userAmount.value);
+   productTitle.value="";
+   userAmount.value="";
+});
+
+
+
+
